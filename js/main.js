942a64a861b9fe92fb7f5fb66aa255738b045ce5
Patrick Passarella <patrick.passarella@BERTON.local>
2018-01-30 18:26:33 -0200

    Updated JS and andd Btn

--- js/main.js
+++ js/main.js
@@ -1,26 +1,45 @@
 let line = document.getElementById('line-one');
 let number = document.getElementById('number');
 let space = document.getElementById('space');
+let submitBtn = document.getElementById('button-js');
 
-number.addEventListener('input', () => {
-    let arr = [];
-    
-    for (let i  = 0; i < number.value; i++) {
-        arr.push(i + 1);
-        let lineNumber = document.createElement('div');
-        lineNumber.classList.add('line-number');
-        lineNumber.innerHTML = arr[i];
-        
-        line.appendChild(lineNumber);
-        
-        if (i == undefined) {
-            while (line.hasChildNodes()) {
-                line.removeChild(line.lastChild);
-            }
-            arr.splice(0, arr.length);
-        }
-        
-    }
-    
-    console.log(typeof(i));    
+//number.addEventListener('input', (e) => {
+//    let arr = [];
+//    
+//    for (let i  = 0; i < number.value; i++) {
+//        arr.push(i + 1);
+//        let lineNumber = document.createElement('div');
+//        lineNumber.classList.add('line-number');
+//        lineNumber.innerHTML = arr[i];
+//        
+//        line.appendChild(lineNumber);
+//        
+//        if (number.value.isUndefined == true) {
+//            while (line.hasChildNodes()) {
+//                line.removeChild(line.lastChild);
+//            }
+//            arr.splice(0, arr.length);
+//        }
+//		
+//		console.log(typeof(number.value))
+//        
+//    }
+//    
+//    console.log(typeof(i));    
+//});
+
+submitBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	let arr = [];
+	
+	for (let i = 0; i < number.value; i++) {
+		arr.push(i + 1);
+		
+		let lineNumber = document.createElement('div');
+		lineNumber.classList.add('line-number');
+		lineNumber.innerHTML = arr[i];
+		
+		line.appendChild(lineNumber);
+	}
+	
 });
